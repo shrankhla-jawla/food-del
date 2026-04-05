410ccf1c04d6265726345ee4244255cc7ad7826b
shrankhla-jawla <shrankjawla08@gmail.com>
2026-04-05 21:50:22 +0530

    fix case issue

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from "react";
 import "./PlaceOrder.css";
-import { StoreContext } from "../../temp/StoreContext";
+import { StoreContext } from "../../context/StoreContext";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
