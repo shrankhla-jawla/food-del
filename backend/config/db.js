08f2ed47f2100186141f665a459a69d9b16f03f3
shrankhla-jawla <shrankjawla08@gmail.com>
2026-04-05 16:49:50 +0530

    fix: resolve static asset path for logo in production build

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect(process.env.MONGODB_URI).then(()=>console.log("DB Connected"));
+    await mongoose.connect(process.env.STRIPE_SECRET_KEY).then(()=>console.log("DB Connected"));
    
 }
 
